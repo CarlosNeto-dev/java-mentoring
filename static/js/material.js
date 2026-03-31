8ff8dbc96131370eb7e5db18c21c67f7343601a2
Carlos Alberto da Silva Neto <netoc4533@gmail.com>
2026-03-30 22:19:28 -0300

    New Content and Fixes

--- static/js/material.js
+++ static/js/material.js
@@ -2,8 +2,15 @@
 
 const materialNavbarMobile = document.querySelector(".navbar");
 const materialBarsMobileBtn = document.querySelector(".mobile-btn");
+const arrowIcon = document.querySelectorAll(".arrow-icon");
 
 materialBarsMobileBtn.addEventListener("click", () => {
   materialNavbarMobile.classList.toggle("active");
   materialBarsMobileBtn.classList.toggle("active");
 })
+
+arrowIcon.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    icon.classList.toggle("active");
+  });
+});
