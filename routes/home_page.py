d15b5e70cd52fcdc72ccba57e9c34b7569440ec8
Carlos Alberto da Silva Neto <netoc4533@gmail.com>
2026-04-04 04:15:28 -0300

    About Us Page

--- routes/home_page.py
+++ routes/home_page.py
@@ -9,3 +9,10 @@ def home():
     """Return for front-end the home-page html"""
 
     return render_template("home/index.html")
+
+
+@home_page_bp.route("/about_us", methods = ["GET"])
+def about_us():
+    """Return for front-end the about-page html"""
+
+    return render_template("home/about_us.html")
